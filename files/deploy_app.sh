47741395a60107b79547d27b2837e1123227fafc
TomHowarth <tom.howarth@planetvm.net>
2024-02-27 12:47:56 +0000

    Updated HTML Payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  HashiCat will rule the world - Terraform cloud for the win. 
   <!-- END -->
   
   </div>
